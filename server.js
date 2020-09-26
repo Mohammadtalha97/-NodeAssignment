16994a25d1b2f9f893d68f74bf2e4f3e98de16b3
Mohammadtalha <mohammadtalha.patel@thegatewaycorp.com>
2020-09-26 11:20:00 +0530

    app.use middlewaren apply there

--- server.js
+++ server.js
@@ -5,31 +5,32 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoute.js";
+import { extendTimeoutMiddleware } from "./controllers/authController";
 dotenv.config({ path: "./config/config.env" });
-
 
 const app = new Express();
 //use body-parser
-app.use(cors({origin: "*",credentials: true,}));
+app.use(cors({ origin: "*", credentials: true }));
 
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "200mb" }));
 app.use(bodyParser.urlencoded({ limit: "200mb", extended: true }));
 
+app.use(extendTimeoutMiddleware);
 //connect to database
 connectDB();
 
 //configer for only development
 // if (process.env.NODE_ENV === "development") {
-  // app.use(
-  //   cors({
-  //     // origin : process.env.CLIENT_URL,
-  //     origin: "*",
-  //     credentials: true,
-  //   })
-  // );
+// app.use(
+//   cors({
+//     // origin : process.env.CLIENT_URL,
+//     origin: "*",
+//     credentials: true,
+//   })
+// );
 
-  //morgan give information about each module
+//morgan give information about each module
 //   app.use(morgan("dev"));
 // }
 
