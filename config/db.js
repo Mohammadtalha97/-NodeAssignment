3ce629340d4164c0e2c2aca36268212138d94ea3
Mohammadtalha <mohammadtalha.patel@thegatewaycorp.com>
2020-09-28 11:55:48 +0530

    mongo atlas url added

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
 const url =
-  "mongodb+srv://talha:#Talha@123@cluster0.irsba.mongodb.net/NodeAPI?retryWrites=true&w=majority";
+  "mongodb+srv://Talha:Talha1234@cluster0.lkdbo.mongodb.net/MyData?retryWrites=true&w=majority";
+// mongodb+srv://Talha:#Talha@123@cluster0.lkdbo.mongodb.net/Usermanagement?retryWrites=true&w=majority
+// ("mongodb+srv://daskh:Daksh1998@cluster0.irsba.mongodb.net/NodeAPI?retryWrites=true&w=majority");Usermanagement
 
 //config.db
 export default async (db) => {
   try {
     const conn = await mongoose.connect(
-      url,
+      // url,
+      process.env.MONGO_ATLAS_URL,
       // process.env.MONGO_URL,
       {
         useNewUrlParser: true,
